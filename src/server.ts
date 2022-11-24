030abb01acf502f15a8b925e4c298da3b4be27de
AmadejKrepek <krepekamadej@gmail.com>
2022-11-24 18:42:18 +0100

    Quick fix

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,6 @@ import { ordersRouter } from "./routes/orders.router";
 
 import { createWeatherRecord, deleteWeatherRecord, getWeatherRecord, updateWeatherRecord } from "./routes/weather-record";
 
-
 // Constants
 const PORT = 8000;
 const HOST = '0.0.0.0';
@@ -16,10 +15,10 @@ const app = express();
 const parser = json();
 
 app.get("/", (req: any, res: any) => {
-  res.status(200).send("hello world!");
+  res.status(200).send("Welcome to Car Order Service API");
 });
 
-app.use('/doc', express.static(__dirname + '/doc'))
+app.use('/doc', express.static('doc'))
 
 app.get("/ping", (req: any, res: any) => {
   res.status(200).send("pong");
@@ -33,11 +32,11 @@ app.get("/env-var", (req: any, res: any) => {
   res.status(200).send(process.env.TEST_ENV_VAR);
 });
 
-app.get('/weather-record/:id', parser, getWeatherRecord)
-app.get('/weather-record', parser, getWeatherRecord)
-app.post('/weather-record', parser, createWeatherRecord)
-app.put('/weather-record/:id', parser, updateWeatherRecord)
-app.delete('/weather-record/:id', parser, deleteWeatherRecord)
+//app.get('/weather-record/:id', parser, getWeatherRecord)
+//app.get('/weather-record', parser, getWeatherRecord)
+//app.post('/weather-record', parser, createWeatherRecord)
+//app.put('/weather-record/:id', parser, updateWeatherRecord)
+//app.delete('/weather-record/:id', parser, deleteWeatherRecord)
 
 connectToDatabase()
   .then(() => {
