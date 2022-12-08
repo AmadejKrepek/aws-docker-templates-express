6c47ebf9e7f63043c470c942deb838704a042879
AmadejKrepek <krepekamadej@gmail.com>
2022-12-08 20:51:55 +0100

    Enabled cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import * as express from "express";
 import { json } from "body-parser";
 
+import * as cors from 'cors';
+
 import { connectToDatabase } from "./config/mongodb"
 import { ordersRouter } from "./routes/orders.router";
 
@@ -13,6 +15,10 @@ const HOST = '0.0.0.0';
 // App handlers
 const app = express();
 const parser = json();
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.get("/", (req: any, res: any) => {
   res.status(200).send("Welcome to Car Order Service API");
